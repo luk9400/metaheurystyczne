f4308b28d81ba50e36058562eeace51437ee147b
luk9400 <lukaszbratos@gmail.com>
2020-05-23 14:39:07 +0200

    fixes

--- list3/z2/task2.py
+++ list3/z2/task2.py
@@ -3,11 +3,17 @@ from copy import deepcopy
 from math import log
 from random import gauss, choice, random, randrange, choices, randint
 from sys import stderr
+from collections import Counter
 
 
 def mutate(s, multiset, dictionary):
+    letters = Counter(s)
+    remaining_dict = {char: multiset[char][1] - letters[char] for char in multiset}
+    chars = [char for char, value in remaining_dict.items() if value > 0]
+    if not chars:
+        return s
+
     p = random()
-    chars = list(multiset.keys())
 
     if p < 0.2:
         return s + choices(chars, k=randint(1, 2))
@@ -49,47 +55,72 @@ def fitness(s, multiset, dictionary):
         return 0
 
 
-def crossover(a, b):
-    idx_a = randrange(0, len(a))
-    idx_b = randrange(0, len(b))
+# def crossover(a, b):
+#     # idx_a = randrange(0, len(a))
+#     # idx_b = randrange(0, len(b))
 
-    p_a, p_b = a[:idx_a] + b[idx_b:], a[idx_a:] + b[:idx_b]
+#     # p_a, p_b = a[:idx_a] + b[idx_b:], a[idx_a:] + b[:idx_b]
 
-    for i in range(min(len(p_a), len(p_b))):
-        if random() < 0.2:
-            p_a[i], p_b[i] = p_b[i], p_a[i]
+#     for i in range(min(len(a), len(b))):
+#         if random() < 0.1:
+#             a[i], b[i] = b[i], a[i]
 
-    return p_a, p_b
+#     return a, b
 
+def twoPointCrossover(pa, pb):
+    i1a, i2a = choices(range(len(pa)), k=2)
+    i1b, i2b = choices(range(len(pb)), k=2)
+
+    if i1a > i2a:
+        i1a, i2a = i2a, i1a
+
+    if i1b > i2b:
+        i1b, i2b = i2b, i1b
+    
+    return pa[:i1a] + pb[i1b:i2b] + pa[i2a:], pb[:i1b] + pa[i1a:i2a] + pb[i2b:]
+
+def crossover(pa, pb):
+    r = random()
+    if r < 0.5:
+        return twoPointCrossover(pa, pb)
+    newpa = pa
+    newpb = pb
+    l = min(len(newpa), len(newpb))
+    for i in range(l):
+        if random() < 1 / l:
+            newpa[i], newpb[i] = newpb[i], newpa[i]
+
+    return newpa, newpb
 
 def genetic_algorithm(t, n, initial_candidates, multiset, dictionary):
     _fitness = lambda x: fitness(x, deepcopy(multiset), dictionary)
     _tournament = lambda x: tournament(x, multiset, dictionary)
 
-    popsize = 40
+    popsize = 10
+    elite = 4
 
     P = initial_candidates
 
     best = None
     start = time()
     while time() - start < t:
+        allowed = [p for p in P if "".join(p) in dictionary and _fitness(p) != 0]
+        # allowed = list(set(allowed))
+        allowed.sort(key=lambda x: _fitness(x), reverse=True)
 
-        allowed = [p for p in P if "".join(p) in dictionary]
         for p in allowed:
             if best == None or _fitness(p) > _fitness(best):
                 best = p
                 last_best = time()
 
-        allowed.sort(key=lambda x: _fitness(x), reverse=True)
-
-        Q = allowed[: popsize // 2]
-        while len(Q) < popsize:
+        Q = allowed[:elite]
+        for _ in range((popsize - elite) // 2):
             p_a = _tournament(deepcopy(allowed))
             p_b = _tournament(deepcopy(allowed))
             c_a, c_b = crossover(deepcopy(p_a), deepcopy(p_b))
             Q.append(mutate(c_a, multiset, dictionary))
             Q.append(mutate(c_b, multiset, dictionary))
-        P = Q[:popsize]
+        P = Q
 
         if time() - last_best > log(t):
             break
